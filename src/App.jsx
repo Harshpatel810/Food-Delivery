630bb12b2453d260bc4bfac2af7f9e58999f1212
vaibhav patil <vaibhavpatil2579@gmail.com>
2025-01-23 19:28:51 +0530

    Title changed to food App

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1 style={{}}>Changes done by vaibhav patil</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
